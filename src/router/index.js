5f1704ec35c1d3ae4aa90a2f357d58b280e5fa86
Celebi63 <fcelebias@student.agh.edu.pl>
2026-01-09 13:58:35 +0100

    Revert GitHub Pages config - deploying to Vercel instead

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import KursPodstawowy from '../views/KursPodstawowy.vue'
 import KursCysterny from '../views/KursCysterny.vue'
@@ -120,7 +120,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes,
   scrollBehavior(to, from, savedPosition) {
     if (savedPosition) {
